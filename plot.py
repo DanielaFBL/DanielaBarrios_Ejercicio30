125f8b7d657bc818d399480617a2dd21732cc47d
DanielaFBL <48950358+DanielaFBL@users.noreply.github.com>
2019-05-09 07:52:20 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -5,7 +5,7 @@ import imageio
 images = []
 x=np.arange(0,1, 0.01)
 
-for j in range(200):
+for i in range(200):
     datos=plt.figure()
     plt.plot(x, np.loadtxt("datos.dat")[i])
     plt.savefig("datos"+str(i)+".png")
