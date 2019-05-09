ed8ed7f80b812a5176e8821eb971eb290b3b96d5
DanielaFBL <48950358+DanielaFBL@users.noreply.github.com>
2019-05-09 07:48:15 -0500

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+
+images = []
+x=np.arange(0,1, 0.01)
+
+for j in range(200):
+    datos=plt.figure()
+    plt.plot(x, np.loadtxt("datos.dat")[i])
+    plt.savefig("datos"+str(i)+".png")
+    plt.close(datos)
+for i in range(200):
+    images.append(imageio.imread("datos"+str(i)+".png")
+imageio.mimsave("imagen.gif", images)
